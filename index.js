24132ad0cff49a75e69af2449a38161f8d272132
gomez50057 <37481091+gomez50057@users.noreply.github.com>
2022-05-09 17:03:31 -0500

    100

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/MissionCommander');
+const Gabriel  = new MissionCommander("Gabriel")
+const pizza = new MissionCommander("pizza")
+const hipotenusa = new MissionCommander("hipotenusa")
+console.log(Gabriel.name)
+console.log(pizza.name)
+console.log(hipotenusa.name)
